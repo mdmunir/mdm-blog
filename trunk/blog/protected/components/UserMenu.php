c8f04c7a503bd7360f3e0a79734e30f7c6136903
misbahuldmunir@gmail.com <misbahuldmunir@gmail.com@1997f77a-227b-a7e5-ad9c-302995f0957d>
2011-12-27 03:05:53 +0000

    edit

--- trunk/blog/protected/components/UserMenu.php
+++ trunk/blog/protected/components/UserMenu.php
@@ -5,31 +5,36 @@ Yii::import('ext.mbmenu.MbMenu');
 class UserMenu extends MbMenu {
 
     public function init() {
-        $itemSearch = array('label' => $this->render('_search',null,TRUE), );
-        if (Yii::app()->user->isGuest) {
-            $this->items = array(
-                $itemSearch,
-                array('label' => 'Login', 'url' => array('site/login')),
-            );
-        } else {
-            $this->items = array(
-                $itemSearch,
-                array('label' => Yii::app()->user->name, 'items' => array(
-                        array('label' => 'change profile', 'url' => array('user/update')),
-                        array('label' => 'change password', 'url' => array('user/changePassword')),
-                        array('label' => 'Logout', 'url' => array('site/logout')),
-                    )
-                ),
-            );
-            if (Yii::app()->user->can_posting) {
-                $this->items[] = array('label' => 'Manage', 'items' => array(
-                        array('label' => 'Create New Post', 'url' => array('post/create')),
-                        array('label' => 'Manage Post', 'url' => array('post/admin')),
-                        array('label' => 'Approve comment (' . Comment::model()->pendingCommentCount . ')',
-                            'url' => array('comment/index')),
-                        ));
-            }
-        }
+        $isAuth = !Yii::app()->user->isGuest;
+        $uName = $isAuth ? Yii::app()->user->name : 'Guest';
+        $canPost = $isAuth && Yii::app()->user->can_posting;
+        $pendingCount = $canPost ? Comment::model()->pendingCommentCount : 0;
+        $this->items = array(
+            array('label' => $this->render('_search', null, TRUE), 'visible' => false),
+            array('label' => 'Guest', 'visible' => !$isAuth,
+                'items' => array(
+                    array('label'=>'Login','url'=>array('site/login')),
+                    array('label'=>'Register','url'=>array('user/create')),
+                )
+            ),
+            array('label' => $uName, 'visible' => $isAuth,
+                'items' => array(
+                    array('label' => 'change profile', 'url' => array('user/update')),
+                    array('label' => 'change password', 'url' => array('user/changePassword')),
+                    array('label' => 'Administrator', 'visible' => $isAuth && Yii::app()->user->name == 'admin',
+                        'url' => array('admin/index')
+                    ),
+                    array('label' => 'Logout', 'url' => array('site/logout')),
+                )
+            ),
+            array('label' => 'Manage', 'visible' => $canPost,
+                'items' => array(
+                    array('label' => 'Create New Post', 'url' => array('post/create')),
+                    array('label' => 'Manage Post', 'url' => array('post/admin')),
+                    array('label' => "Approve comment ($pendingCount)",
+                        'url' => array('comment/index')),
+            )),
+        );
         parent::init();
     }
 
